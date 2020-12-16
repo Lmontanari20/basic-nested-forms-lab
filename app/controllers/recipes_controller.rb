f7538aaf8fe118db457467abbb505ce599b4ff9f
Lucas Montanari <Lmonty2011@gmail.com>
2020-12-16 11:30:48 -0800

    implemented nested forms

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build()
+    @recipe.ingredients.build()
   end
 
   def create
+    @recipe = Recipe.create(set_params)
+    redirect_to @recipe
+  end
+
+  private 
+
+  def set_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [
+      :name, 
+      :quantity
+    ])
   end
 end
